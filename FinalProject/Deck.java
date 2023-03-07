1cb852715d0577710c9a8c456f15010df4a21378
Narfjones <38131442+Narfjones@users.noreply.github.com>
2023-03-07 13:00:18 -0500

    update

---
+++ FinalProject/Deck.java
@@ -0,0 +1,14 @@
+package FinalProject;
+import java.util.*;
+
+public class Deck {
+    private HashMap<Integer, String> cards = new HashMap();
+
+    public Deck(Card c) {
+        cards.put()
+    }
+
+
+
+
+}
